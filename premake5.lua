3c9e69fec34c410f9e118564df3f8cfa41ff5b43
Kamil Zmich <kamil.zmich@gmail.com>
2014-05-21 21:21:19 +0100

    Premake script

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+project "libogg"
+	version = "1.3.1"
+
+	language "C"
+	
+	files { "src/*.c" }
+	includedirs { "include/" }
+	
+	kind ( LIB_LIBOGG.KIND )
+	
+	targetname( LIB_LIBOGG.KIND == "StaticLib" and "libogg_static" or "libogg" )
+	
+	configuration {"SharedLib", "windows"}
+		linkoptions { "/DEF:\"../../Source/Thirdparty/libogg/win32/ogg.def\"" }
+	configuration {}
+	
+	targetdir( LDIR_THIRDPARTY_LIB )
+	location( LDIR_THIRDPARTY_BUILD )
+
+	suffix_macro ( version, true )
